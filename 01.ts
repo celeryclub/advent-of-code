adfed244b56b3526b77cbb6bc6f1f52ba24d1b0c
Steve Davis <steve@celery.club>
2018-12-13 14:28:44 -0500

    Solve problem 2

--- 01.ts
+++ 01.ts
@@ -17,4 +17,31 @@ async function _01() {
   console.log(total);
 }
 
+async function _02() {
+  const numbers = await _getNumbers();
+
+  let frequency = 0;
+  let index = 0;
+  let knownFrequencies = {};
+
+  while (true) {
+    frequency = frequency + numbers[index];
+
+    if (knownFrequencies[frequency.toString()]) {
+      console.log(frequency);
+      return frequency;
+    }
+
+    knownFrequencies[frequency.toString()] = true;
+
+    if (index < numbers.length - 1) {
+      index++;
+    }
+    else {
+      index = 0;
+    }
+  }
+}
+
 _01();
+_02();
