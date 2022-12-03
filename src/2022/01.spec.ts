62e74fe7837073084af7693151a60a926a5f53ac
Steve Davis <steve@celery.club>
2022-12-02 18:06:45 -0800

    Add sample input to 2022 tests

--- src/2022/01.spec.ts
+++ src/2022/01.spec.ts
@@ -1,11 +1,21 @@
 import { readLines } from "../lib/read";
 import { Solver } from "./01";
 
-const fullInput = readLines("2022/01");
+const sampleInput = readLines("2022/01-sample");
+const fullInput = readLines("2022/01-full");
 
 describe("01", () => {
-  const solver = new Solver(fullInput);
+  describe("sample input", () => {
+    const solver = new Solver(sampleInput);
 
-  test("part 1", () => expect(solver.part1()).toBe(71023));
-  test("part 2", () => expect(solver.part2()).toBe(206289));
+    test("part 1", () => expect(solver.part1()).toBe(24000));
+    test("part 2", () => expect(solver.part2()).toBe(45000));
+  });
+
+  describe("full input", () => {
+    const solver = new Solver(fullInput);
+
+    test("part 1", () => expect(solver.part1()).toBe(71023));
+    test("part 2", () => expect(solver.part2()).toBe(206289));
+  });
 });
--- src/2022/02.spec.ts
+++ src/2022/02.spec.ts
@@ -1,11 +1,21 @@
 import { readLines } from "../lib/read";
 import { Solver } from "./02";
 
-const fullInput = readLines("2022/02");
+const sampleInput = readLines("2022/02-sample");
+const fullInput = readLines("2022/02-full");
 
 describe("02", () => {
-  const solver = new Solver(fullInput);
+  describe("sample input", () => {
+    const solver = new Solver(sampleInput);
 
-  test("part 1", () => expect(solver.part1()).toBe(10816));
-  test("part 2", () => expect(solver.part2()).toBe(11657));
+    test("part 1", () => expect(solver.part1()).toBe(15));
+    test("part 2", () => expect(solver.part2()).toBe(12));
+  });
+
+  describe("full input", () => {
+    const solver = new Solver(fullInput);
+
+    test("part 1", () => expect(solver.part1()).toBe(10816));
+    test("part 2", () => expect(solver.part2()).toBe(11657));
+  });
 });
