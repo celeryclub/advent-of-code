de056808df6cc2175fa9762ea48adbfb4511c063
Steve Davis <steve@celery.club>
2022-12-05 11:23:41 -0800

    Only trim the end of input strings

--- src/lib/read.ts
+++ src/lib/read.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 
 export function read(inputPath: string): string {
   const filePath = path.join(__dirname, "../..", "input", `${inputPath}.txt`);
-  return fs.readFileSync(filePath, "utf8").trim();
+  return fs.readFileSync(filePath, "utf8").trimEnd();
 }
 
 export function readLines(inputPath: string): string[] {
