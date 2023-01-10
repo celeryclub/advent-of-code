19e4d18081191e603738035bfe562ebb0a1b5a41
Steve Davis <steve@celery.club>
2023-01-10 11:10:53 -0800

    Clean up problem 2022/04 using itertools

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,17 +1,17 @@
 // https://adventofcode.com/2022/day/4
 
+use itertools::Itertools;
 use regex::Regex;
 use std::fs::read_to_string;
 
-fn parse_line(re: &Regex, line: &str) -> [u16; 4] {
+fn parse_line(re: &Regex, line: &str) -> (u16, u16, u16, u16) {
     // https://docs.rs/regex/latest/regex/#example-avoid-compiling-the-same-regex-in-a-loop
     re.captures(line)
         .unwrap()
         .iter()
         .skip(1)
         .map(|m| m.unwrap().as_str().parse::<u16>().unwrap())
-        .collect::<Vec<u16>>()
-        .try_into()
+        .collect_tuple()
         .unwrap()
 }
 
@@ -21,7 +21,7 @@ fn part1(input: &str) -> u16 {
     input
         .lines()
         .filter(|line| {
-            let [start1, end1, start2, end2] = parse_line(&re, line);
+            let (start1, end1, start2, end2) = parse_line(&re, line);
             (start1 <= start2 && end1 >= end2) || (start2 <= start1 && end2 >= end1)
         })
         .collect::<Vec<&str>>()
@@ -34,7 +34,7 @@ fn part2(input: &str) -> u16 {
     input
         .lines()
         .filter(|line| {
-            let [start1, end1, start2, end2] = parse_line(&re, line);
+            let (start1, end1, start2, end2) = parse_line(&re, line);
             (start1 <= start2 && end1 >= start2) || (start2 <= start1 && end2 >= start1)
         })
         .collect::<Vec<&str>>()
