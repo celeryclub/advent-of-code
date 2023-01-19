381e8a1e0fe8cd51ee2b053f7dd1a465360bc75b
Steve Davis <steve@celery.club>
2023-01-18 19:00:07 -0800

    Rewrite problem 2018/01 in Rust

---
+++ 2018/src/bin/01.rs
@@ -0,0 +1,29 @@
+// https://adventofcode.com/2018/day/1
+
+use std::collections::HashSet;
+
+fn part1(input: &str) -> i32 {
+    input.lines().map(|line| line.parse::<i32>().unwrap()).sum()
+}
+
+fn part2(input: &str) -> i32 {
+    let mut known_frequencies = HashSet::new();
+
+    input
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .cycle()
+        .scan(0, |freq, change| {
+            *freq += change;
+            Some(*freq)
+        })
+        .find(|freq| !known_frequencies.insert(*freq))
+        .unwrap()
+}
+
+fn main() {
+    let input = include_str!("../../input/01.txt").trim_end();
+
+    println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
+}
