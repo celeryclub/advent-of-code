42514035578e6ba6f2f2b3d25c4cc1f026dbe1e1
Steve Davis <steve@celery.club>
2023-01-20 11:35:29 -0800

    Solve problem 2015/01

---
+++ 2015/src/bin/01.rs
@@ -0,0 +1,31 @@
+// https://adventofcode.com/2015/day/1
+
+fn part1(input: &str) -> i16 {
+    input
+        .chars()
+        .fold(0, |floor, char| if char == '(' { floor + 1 } else { floor - 1 })
+}
+
+fn part2(input: &str) -> usize {
+    input
+        .chars()
+        // scan is actually filter_scan which is why its iterator yields an option
+        .scan(0, |floor, char| {
+            if char == '(' {
+                *floor += 1;
+            } else {
+                *floor -= 1;
+            }
+            Some(*floor)
+        })
+        .position(|floor| floor < 0)
+        .unwrap()
+        + 1
+}
+
+fn main() {
+    let input = include_str!("../../input/01.txt").trim_end();
+
+    println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
+}
