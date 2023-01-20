198a0b65881549c77cc58c09cb4d9657938bbacd
Steve Davis <steve@celery.club>
2023-01-19 20:34:13 -0800

    Remove example input files

--- 2022/src/bin/02.rs
+++ 2022/src/bin/02.rs
@@ -43,7 +43,7 @@ fn part2(input: &str) -> u16 {
 }
 
 fn main() {
-    let input = include_str!("../../input/02-full.txt").trim_end();
+    let input = include_str!("../../input/02.txt").trim_end();
 
     println!("part 1: {}", part1(input));
     println!("part 2: {}", part2(input));
