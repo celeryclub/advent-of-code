31c95405060b5fd9f4e6d3ba6bf545cce6cafe2c
Steve Davis <steve@celery.club>
2023-01-21 15:59:40 -0800

    Refactor problem 2015/04 for better performance
    The prefix only needs to be hashed once.

--- 2015/src/bin/04.rs
+++ 2015/src/bin/04.rs
@@ -1,25 +1,33 @@
 // https://adventofcode.com/2015/day/4
 
-use md5;
+use crypto::{digest::Digest, md5::Md5};
 
-fn part1(input: &str) -> u32 {
+fn lowest_suffix_with_zeros(input: &str, zeros: usize) -> u32 {
+    let mut prefix_hash = Md5::new();
+    prefix_hash.input_str(input);
+
+    // This is for storing our computed digest later
+    let mut digest = vec![0u8; 16];
+
     (1..)
         .find(|n| {
-            let digest = md5::compute(format!("{}{}", input, n));
-            // The first two bytes should be 0, and the third should be < 0x10
-            digest.iter().take(2).all(|b| *b == 0) && *digest.iter().nth(2).unwrap() < 0x10
+            let mut hash = prefix_hash; // Md5 implements Copy
+            hash.input_str(&n.to_string());
+            hash.result(&mut digest);
+
+            // All full bytes must be 0, and any half bytes must be < 0x10
+            digest.iter().take(zeros / 2).all(|b| *b == 0)
+                && (zeros % 2 == 0 || *digest.iter().nth(zeros / 2).unwrap() < 0x10)
         })
         .unwrap()
 }
 
+fn part1(input: &str) -> u32 {
+    lowest_suffix_with_zeros(input, 5)
+}
+
 fn part2(input: &str) -> u32 {
-    (1..)
-        .find(|n| {
-            let digest = md5::compute(format!("{}{}", input, n));
-            // The first three bytes should be 0
-            digest.iter().take(3).all(|b| *b == 0)
-        })
-        .unwrap()
+    lowest_suffix_with_zeros(input, 6)
 }
 
 fn main() {
