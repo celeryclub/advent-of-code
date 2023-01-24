6ae1d6fe00666f7e399121b844f38cbb8362061e
Steve Davis <steve@celery.club>
2023-01-23 18:55:46 -0800

    Refactor problem 2018/03 part 1 for brevity

--- 2018/src/bin/03.rs
+++ 2018/src/bin/03.rs
@@ -1,45 +1,31 @@
 // https://adventofcode.com/2018/day/3
 
+use itertools::Itertools;
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::HashMap;
 
-#[derive(Debug)]
-struct Rect {
-    x: u16,
-    y: u16,
-    width: u16,
-    height: u16,
-}
-
-fn parse_line(re: &Regex, line: &str) -> Rect {
-    let caps = re.captures(line).unwrap();
-
-    Rect {
-        x: caps[1].parse::<u16>().unwrap(),
-        y: caps[2].parse::<u16>().unwrap(),
-        width: caps[3].parse::<u16>().unwrap(),
-        height: caps[4].parse::<u16>().unwrap(),
-    }
+fn parse_line(re: &Regex, line: &str) -> (u16, u16, u16, u16, u16) {
+    re.find_iter(line)
+        .map(|m| m.as_str().parse::<u16>().unwrap())
+        .collect_tuple()
+        .unwrap()
 }
 
 fn part1(input: &str) -> usize {
-    let re = Regex::new(r"(\d+),(\d+): (\d+)x(\d+)").unwrap();
-    let mut claims = HashSet::new();
-    let mut disputed_claims = HashSet::new();
+    let re = Regex::new(r"(\d+)").unwrap();
+    let mut claims: HashMap<(u16, u16), u16> = HashMap::new();
 
     input.lines().for_each(|line| {
-        let rect = parse_line(&re, line);
+        let (_, left, top, width, height) = parse_line(&re, line);
 
-        for x in rect.x..rect.x + rect.width {
-            for y in rect.y..rect.y + rect.height {
-                if !claims.insert((x, y)) {
-                    disputed_claims.insert((x, y));
-                }
+        for x in left..left + width {
+            for y in top..top + height {
+                *claims.entry((x, y)).or_default() += 1;
             }
         }
     });
 
-    disputed_claims.len()
+    claims.values().filter(|&v| *v > 1).count()
 }
 
 fn main() {
