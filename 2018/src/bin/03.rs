d396e1f4b0674060306e2217257bccdf308bfce7
Steve Davis <steve@celery.club>
2023-01-23 22:54:34 -0800

    Solve problem 2018/03 part 2

--- 2018/src/bin/03.rs
+++ 2018/src/bin/03.rs
@@ -2,34 +2,55 @@
 
 use itertools::Itertools;
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-fn parse_line(re: &Regex, line: &str) -> (u16, u16, u16, u16, u16) {
-    re.find_iter(line)
-        .map(|m| m.as_str().parse::<u16>().unwrap())
-        .collect_tuple()
-        .unwrap()
-}
-
-fn part1(input: &str) -> usize {
+fn mark_claims(input: &str) -> HashMap<(u16, u16), HashSet<u16>> {
     let re = Regex::new(r"(\d+)").unwrap();
-    let mut claims: HashMap<(u16, u16), u16> = HashMap::new();
+    let mut claimed: HashMap<(u16, u16), HashSet<u16>> = HashMap::new();
 
     input.lines().for_each(|line| {
-        let (_, left, top, width, height) = parse_line(&re, line);
+        let (id, left, top, width, height) = re
+            .find_iter(line)
+            .map(|m| m.as_str().parse::<u16>().unwrap())
+            .collect_tuple()
+            .unwrap();
 
         for x in left..left + width {
             for y in top..top + height {
-                *claims.entry((x, y)).or_default() += 1;
+                claimed.entry((x, y)).or_default().insert(id);
             }
         }
     });
 
-    claims.values().filter(|&v| *v > 1).count()
+    claimed
+}
+
+fn part1(input: &str) -> usize {
+    let claimed = mark_claims(input);
+
+    claimed.values().filter(|ids| ids.len() > 1).count()
+}
+
+fn part2(input: &str) -> u16 {
+    let claimed = mark_claims(input);
+
+    let mut all_claims: HashSet<u16> = HashSet::new();
+    let mut contested_claims: HashSet<u16> = HashSet::new();
+
+    claimed.values().for_each(|ids| {
+        all_claims.extend(ids);
+
+        if ids.len() > 1 {
+            contested_claims.extend(ids);
+        }
+    });
+
+    *(&all_claims - &contested_claims).iter().next().unwrap()
 }
 
 fn main() {
     let input = include_str!("../../input/03.txt").trim_end();
 
     println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
 }
