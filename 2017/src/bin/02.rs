3361a27ecb8dccbad8a282cd8d834091bea32b54
Steve Davis <steve@celery.club>
2023-04-12 15:55:29 -1000

    Solve problem 2017/02

---
+++ 2017/src/bin/02.rs
@@ -0,0 +1,44 @@
+// https://adventofcode.com/2017/day/2
+
+fn parse_rows(input: &str) -> impl Iterator<Item = Vec<u16>> + '_ {
+    input.lines().map(|line| {
+        line.split('\t')
+            .map(|n| n.parse::<u16>().unwrap())
+            .collect::<Vec<u16>>()
+    })
+}
+
+fn part1(input: &str) -> u16 {
+    parse_rows(input)
+        .map(|row| row.iter().max().unwrap() - row.iter().min().unwrap())
+        .sum()
+}
+
+fn part2(input: &str) -> u16 {
+    parse_rows(input)
+        .map(|row| {
+            for i in 0..row.len() {
+                for j in i + 1..row.len() {
+                    let (a, b) = if row[i] < row[j] {
+                        (row[j], row[i])
+                    } else {
+                        (row[i], row[j])
+                    };
+
+                    if a % b == 0 {
+                        return a / b;
+                    }
+                }
+            }
+
+            0
+        })
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("../../input/02.txt").trim_end();
+
+    println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
+}
