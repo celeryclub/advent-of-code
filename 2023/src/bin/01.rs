be7673b9500d2deb87a01143d8b98169c30a6679
Steve Davis <steve@celery.club>
2023-12-03 14:40:03 -0800

    Solve problem 2023/01 part 2

--- 2023/src/bin/01.rs
+++ 2023/src/bin/01.rs
@@ -1,6 +1,7 @@
 // https://adventofcode.com/2023/day/1
 
 use either::Either;
+use regex::Regex;
 use std::{iter::Rev, str::Chars};
 
 fn find_digit(chars: Either<Chars, Rev<Chars>>) -> u32 {
@@ -11,6 +12,44 @@ fn find_digit(chars: Either<Chars, Rev<Chars>>) -> u32 {
                 Some(char.to_digit(10).unwrap())
             } else {
                 None
+            }
+        })
+        .unwrap()
+}
+
+fn find_digit_or_word(chars: Either<Chars, Rev<Chars>>, re: &Regex) -> u32 {
+    let direction_forward = chars.is_left();
+    let mut buffer = String::new();
+
+    chars
+        .into_iter()
+        .find_map(|char| {
+            if char.is_numeric() {
+                Some(char.to_digit(10).unwrap())
+            } else {
+                if direction_forward {
+                    buffer.push(char);
+                } else {
+                    buffer.insert(0, char);
+                }
+
+                let re_match = re.find(&buffer);
+                if re_match.is_some() {
+                    match re_match.unwrap().as_str() {
+                        "one" => Some(1),
+                        "two" => Some(2),
+                        "three" => Some(3),
+                        "four" => Some(4),
+                        "five" => Some(5),
+                        "six" => Some(6),
+                        "seven" => Some(7),
+                        "eight" => Some(8),
+                        "nine" => Some(9),
+                        _ => unreachable!(),
+                    }
+                } else {
+                    None
+                }
             }
         })
         .unwrap()
@@ -28,9 +67,23 @@ fn part1(input: &str) -> u32 {
         .sum::<u32>()
 }
 
+fn part2(input: &str) -> u32 {
+    let re = Regex::new(r"(one|two|three|four|five|six|seven|eight|nine)").unwrap();
+
+    input
+        .lines()
+        .map(|line| {
+            let first_digit = find_digit_or_word(Either::Left(line.chars()), &re);
+            let last_digit = find_digit_or_word(Either::Right(line.chars().rev()), &re);
+
+            first_digit * 10 + last_digit
+        })
+        .sum::<u32>()
+}
+
 fn main() {
     let input = include_str!("../../input/01.txt").trim_end();
 
     println!("part 1: {}", part1(input));
-    // println!("part 2: {}", part2(input));
+    println!("part 2: {}", part2(input));
 }
