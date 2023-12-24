93babe2c507430ab511f0cef5f5b72c39a2c5659
Steve Davis <steve@celery.club>
2023-12-24 12:55:38 -0800

    Add tests to problem 2017/05

--- 2017/src/bin/05.rs
+++ 2017/src/bin/05.rs
@@ -57,3 +57,18 @@ fn main() {
     println!("part 1: {}", part1(input));
     println!("part 2: {}", part2(input));
 }
+
+#[cfg(test)]
+mod tests {
+    const INPUT: &str = include_str!("../../input/05.txt");
+
+    #[test]
+    fn part1() {
+        assert_eq!(super::part1(INPUT.trim_end()), 355965);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(super::part2(INPUT.trim_end()), 26948068);
+    }
+}
