240c0e5744aea69270b727134959d7ba2eb400fc
Steve Davis <steve@celery.club>
2023-12-30 20:35:03 -0800

    Solve problem 2020/09 part 2

--- 2020/src/bin/09.rs
+++ 2020/src/bin/09.rs
@@ -17,8 +17,8 @@ fn part1(input: &str) -> i64 {
 
             if previous
                 .iter()
-                .find(|&&a| {
-                    let difference: i64 = number as i64 - a;
+                .find(|&a| {
+                    let difference: i64 = number as i64 - *a;
 
                     if previous.contains(&difference) {
                         true
@@ -39,10 +39,41 @@ fn part1(input: &str) -> i64 {
         .unwrap()
 }
 
+fn part2(input: &str) -> i64 {
+    let values = input
+        .lines()
+        .map(|line| line.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+
+    let mut start_index = 0;
+    let mut end_index = 1;
+    let mut candidates = VecDeque::from([values[start_index], values[end_index]]);
+    let mut sum = values[start_index] + values[end_index];
+
+    loop {
+        if sum == 2089807806 {
+            break;
+        }
+
+        if sum < 2089807806 {
+            end_index += 1;
+            candidates.push_back(values[end_index]);
+            sum += values[end_index];
+        } else {
+            sum -= values[start_index];
+            candidates.pop_front();
+            start_index += 1;
+        }
+    }
+
+    candidates.iter().min().unwrap() + candidates.iter().max().unwrap()
+}
+
 fn main() {
     let input = include_str!("../../input/09.txt").trim_end();
 
     println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
 }
 
 #[cfg(test)]
@@ -52,5 +83,10 @@ mod tests {
     #[test]
     fn part1() {
         assert_eq!(super::part1(INPUT.trim_end()), 2089807806);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(super::part2(INPUT.trim_end()), 245848639);
     }
 }
