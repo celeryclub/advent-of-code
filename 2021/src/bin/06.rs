c90cf0501d136ddbe637548b93b096a970f5f699
Steve Davis <steve@celery.club>
2024-02-25 13:37:13 -0800

    Solve problem 2021/06 part 2

--- 2021/src/bin/06.rs
+++ 2021/src/bin/06.rs
@@ -1,33 +1,60 @@
 // https://adventofcode.com/2021/day/6
 
-fn part1(input: &str) -> usize {
-    let mut fishes = input
+use std::collections::HashMap;
+
+fn run_simulation_for_n_days(input: &str, days: u16) -> u64 {
+    let mut fish_by_cycle: HashMap<u8, u64> = HashMap::from([
+        (0, 0),
+        (1, 0),
+        (2, 0),
+        (3, 0),
+        (4, 0),
+        (5, 0),
+        (6, 0),
+        (7, 0),
+        (8, 0),
+    ]);
+
+    input
         .split(",")
-        .map(|n| n.parse::<u16>().unwrap())
-        .collect::<Vec<u16>>();
+        .for_each(|n| *fish_by_cycle.get_mut(&n.parse().unwrap()).unwrap() += 1);
 
-    for _ in 0..80 {
-        let mut new_fishes = vec![];
+    for _ in 0..days {
+        // We'll start with 0 since there is no cycle 9
+        let mut previous_cycle_count = 0;
 
-        fishes.iter_mut().for_each(|fish| {
-            if *fish == 0 {
-                *fish = 6;
-                new_fishes.push(8);
-            } else {
-                *fish -= 1;
-            }
-        });
+        // Modify each cycle group to become the previous cycle group
+        for cycle in (0..=8).rev() {
+            let this_cycle_count_ref = fish_by_cycle.get_mut(&cycle).unwrap();
+            let this_cycle_count = *this_cycle_count_ref;
 
-        fishes.append(&mut new_fishes);
+            *this_cycle_count_ref = previous_cycle_count;
+            previous_cycle_count = this_cycle_count;
+        }
+
+        // Each fish that spawned a new fish will become cycle 6
+        *fish_by_cycle.get_mut(&6).unwrap() += previous_cycle_count;
+
+        // Spawn new fish
+        *fish_by_cycle.get_mut(&8).unwrap() = previous_cycle_count;
     }
 
-    fishes.len()
+    fish_by_cycle.values().sum()
+}
+
+fn part1(input: &str) -> u64 {
+    run_simulation_for_n_days(input, 80)
+}
+
+fn part2(input: &str) -> u64 {
+    run_simulation_for_n_days(input, 256)
 }
 
 fn main() {
     let input = include_str!("../../input/06.txt").trim_end();
 
     println!("part 1: {}", part1(input));
+    println!("part 2: {}", part2(input));
 }
 
 #[cfg(test)]
@@ -37,5 +64,10 @@ mod tests {
     #[test]
     fn part1() {
         assert_eq!(super::part1(INPUT.trim_end()), 360268);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(super::part2(INPUT.trim_end()), 1632146183902);
     }
 }
