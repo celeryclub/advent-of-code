698d07df543b866a8885663c8694561fdca8522c
Steve Davis <steve@celery.club>
2024-02-29 16:42:17 -0800

    Solve problem 2021/04 part 1

---
+++ 2021/src/bin/04.rs
@@ -0,0 +1,108 @@
+// https://adventofcode.com/2021/day/4
+
+// If you run this a bunch of times fast, it will fail occasionally. Wut?????
+// rustc v1.76.0, edition 2021
+
+use std::collections::HashSet;
+
+type Board = Vec<HashSet<u8>>;
+
+fn create_boards(input: &str) -> Vec<Board> {
+    // Boards are represented as sets of possible wins - both rows and columns
+    // Every cell gets duplicated, but that's ok
+    input
+        .split("\n\n")
+        .skip(1)
+        .map(|board_data| {
+            // Add rows
+            let mut board = board_data
+                .split('\n')
+                .map(|row| {
+                    let row_vec = row
+                        .split_whitespace()
+                        .map(|cell| cell.parse::<u8>().unwrap())
+                        .collect::<Vec<_>>();
+
+                    HashSet::from_iter(row_vec)
+                })
+                .collect::<Vec<_>>();
+
+            // Add columns by transposing the rows 90 degrees
+            // This transposition code only works with square matrices
+            let mut columns: Vec<HashSet<u8>> =
+                vec![HashSet::with_capacity(board.len()); board.len()];
+
+            board.iter().for_each(|row| {
+                row.iter().enumerate().for_each(|(j, cell)| {
+                    columns[j].insert(*cell);
+                })
+            });
+
+            board.append(&mut columns);
+
+            board
+        })
+        .collect::<Vec<_>>()
+}
+
+fn mark_number_and_check_for_win(board: &mut Board, turn: u8) -> bool {
+    for line in board.iter_mut() {
+        line.remove(&turn);
+
+        if line.is_empty() {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn calculate_score(board: &Board, turn: u8) -> u32 {
+    // We only want the first 5 lines (the rows).
+    // If we took the columns too, we'd have duplicate cells.
+    let leftover_sum = board
+        .iter()
+        .take(5)
+        .map(|line| line.iter().map(|cell| *cell as u32).sum::<u32>())
+        .sum::<u32>();
+
+    leftover_sum * turn as u32
+}
+
+fn part1(input: &str) -> u32 {
+    let mut boards = create_boards(input);
+
+    let turns = input
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .map(|turn| turn.parse::<u8>().unwrap());
+
+    for turn in turns {
+        for board in boards.iter_mut() {
+            if mark_number_and_check_for_win(board, turn) {
+                // This board wins!
+                return calculate_score(board, turn);
+            }
+        }
+    }
+
+    0
+}
+
+fn main() {
+    let input = include_str!("../../input/04.txt").trim_end();
+
+    println!("part 1: {}", part1(input));
+}
+
+#[cfg(test)]
+mod tests {
+    const INPUT: &str = include_str!("../../input/04.txt");
+
+    #[test]
+    fn part1() {
+        assert_eq!(super::part1(INPUT.trim_end()), 67716);
+    }
+}
