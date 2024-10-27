4c425358f5b52b4c9b986aedccf9a5bf033e3d43
Steve Davis <steve@celery.club>
2024-10-27 15:22:15 -0700

    Add missing tests for some 2016 problems

--- rust/2016/src/bin/05.rs
+++ rust/2016/src/bin/05.rs
@@ -76,3 +76,18 @@ fn main() {
     println!("part 1: {}", part1(input));
     println!("part 2: {}", part2(input));
 }
+
+#[cfg(test)]
+mod tests {
+    const INPUT: &str = "abbhdwsy";
+
+    #[test]
+    fn part1() {
+        assert_eq!(super::part1(INPUT.trim_end()), "801b56a7");
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(super::part2(INPUT.trim_end()), "424a0197");
+    }
+}
